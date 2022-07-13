3d9b3ba3a60dfcda13b48f817bde581eca5399ed
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-07-13 23:26:08 +0300

    feat: some design changes

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,49 +1,58 @@
-import { gsap } from "gsap";
+import { gsap } from 'gsap';
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   let prevScroll = window.pageYOffset;
 
   const tl = gsap.timeline();
 
-  tl.from(".header", {
-    height: "0vh",
+  tl.from('.header', {
+    height: '0vh',
     duration: window.innerWidth > 790 ? 0.4 : 0.5,
   })
-    .from(".header__nav", {
-      opacity: "0",
+    .from('.header__nav', {
+      opacity: '0',
       delay: 0.2,
     })
-    .from(".big-icon", {
-      opacity: "0",
+    .from('.big-icon', {
+      opacity: '0',
       delay: 0.1,
       duration: 0.6,
     })
-    .to(".header", {
-      height: window.innerWidth > 790 ? "60vh" : "40vh",
-      duration: window.innerWidth > 790 ? 0.5 : 0.6,
+    .to('.header', {
+      height: window.innerWidth > 810 ? '60vh' : '40vh',
+      duration: window.innerWidth > 810 ? 0.5 : 0.6,
     })
-    .to(".big-icon", {
-      boxShadow: "3px 3px 4px #00000040",
-      transform: "translate(-3px, -3px)",
+    .to(
+      '.big-icon',
+      {
+        bottom: window.innerWidth > 810 ? '20%' : '34%',
+        delay: 0,
+      },
+      '<'
+    )
+    .to('.big-icon', {
+      boxShadow: '3px 3px 4px #00000040',
+      transform: 'translate(-3px, -3px)',
+
       duration: 0.2,
     })
-    .from(".about-item", {
+    .from('.about-item', {
       opacity: 0,
-      delay: "-1",
+      delay: '-1',
       duration: 0.3,
       stagger: 0.2,
     });
 
-  gsap.from("main", {
-    opacity: "0",
+  gsap.from('main', {
+    opacity: '0',
     delay: 0.6,
   });
-  gsap.from("footer", {
-    opacity: "0",
+  gsap.from('footer', {
+    opacity: '0',
     delay: 0.6,
   });
 
-  gsap.from(".top-products", {
+  gsap.from('.top-products', {
     backgroundSize: 0,
     duration: 1,
     delay: 1,
@@ -56,9 +65,9 @@ window.addEventListener("load", () => {
     // }
 
     if (prevScroll > currentScroll) {
-      document.querySelector(".header__nav").style.top = 0;
+      document.querySelector('.header__nav').style.top = 0;
     } else {
-      document.querySelector(".header__nav").style.top = "-100px";
+      document.querySelector('.header__nav').style.top = '-100px';
     }
     prevScroll = currentScroll;
   };
